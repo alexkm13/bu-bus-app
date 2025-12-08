8ddc77c571f45f828486bfef67ea877ae5a72280
alexkm13 <alexkim9133@gmail.com>
2025-12-08 13:48:52 -0500

    Initial Commit

---
+++ BUBusTracker/BUBusTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUBusTrackerApp.swift
+//  BUBusTracker
+//
+//  Created by Alex Kim on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BUBusTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
